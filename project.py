df05abde59152045d9c95e2fcc9768cdd6c80f87
MarkAlexI <markovaleksandr108@gmail.com>
2025-05-21 12:17:58 +0300

    feat(password): add password generation function

--- project.py
+++ project.py
@@ -1,5 +1,24 @@
+import string
+import random
+
+
+def generate_password(length=12, use_digits=True, use_special_chars=True):
+    characters = string.ascii_letters
+    if use_digits:
+        characters += string.digits
+    if use_special_chars:
+        characters += string.punctuation
+
+    if not characters:
+        raise ValueError("No characters available for password generation.")
+
+    return ''.join(random.choice(characters) for _ in range(length))
+
+
 def main():
-    pass
+    new_password = generate_password()
+    print(new_password)
+
 
 if __name__ == "__main__":
     main()
