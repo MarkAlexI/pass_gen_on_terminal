c1a06a0356bbcf7e73dbd22639bd5f423b9d3b9e
MarkAlexI <markovaleksandr108@gmail.com>
2025-05-23 08:08:42 +0300

    feat(project.py): add function for checking how strong is password

--- project.py
+++ project.py
@@ -15,9 +15,22 @@ def generate_password(length=12, use_digits=True, use_special_chars=True):
     return ''.join(random.choice(characters) for _ in range(length))
 
 
+def is_strong_password(password):
+    has_upper = any(c.isupper() for c in password)
+    has_lower = any(c.islower() for c in password)
+    has_digit = any(c.isdigit() for c in password)
+    has_special = any(c in string.punctuation for c in password)
+    return all([has_upper, has_lower, has_digit, has_special, len(password) >= 8])
+
+
 def main():
     new_password = generate_password()
     print(new_password)
+
+    if is_strong_password(new_password):
+        print("This password is strong.")
+    else:
+        print("Warning: the password is not very secure.")
 
 
 if __name__ == "__main__":
